00089e3cf4191ea1a6f724a705cd88f27ed3399a
Molly Seither <molly.seither@centre.edu>
2018-07-15 17:19:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,7 +28,6 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
-  shipping_manifest["pearl necklace"] = 1
 
 
   # add 4 gun powder to the shipping_manifest hash below
